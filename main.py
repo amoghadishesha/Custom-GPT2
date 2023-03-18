b5a89cb6fec3855210f2089afe721e809f34f4f1
Amogh Adishesha <adisheshaamogh@gmail.com>
2023-03-17 22:54:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def parse_args():
     parser.add_argument('-ep', '--epsilon', help='adam parameter epsilon', default=1e-8, type=float)
     
     parser.add_argument('-tsp', '--text-sample', help='sample text for generation', default="what is", type=str)
-    parser.add_argument('-pm', '--trained-model', help='path to weights', default='./weightsfor100epochs/', type=str)
+    parser.add_argument('-pm', '--trained-model', help='path to weights', default='./trained_weights/', type=str)
     parser.add_argument('-tr', '--train-scratch', help='train from scratch or load weights', default=True, type=bool)
     
     return parser.parse_args()
